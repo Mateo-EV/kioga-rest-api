b20db8b66c0a1570ba668b84370458d478c2600b
Mateo-EV <riojamatthew@gmail.com>
2024-06-18 21:03:17 -0500

    webhook-autoload-fix

---
+++ app/Helpers/QueryHelper.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Helpers;
+
+use Illuminate\Database\Eloquent\Builder;
+
+class QueryHelper
+{
+    public static function buildQueryWithRelations(
+        Builder $query,
+        array $relations,
+        ?string $method = "join"
+    ) {
+        foreach ($relations as $relation) {
+            $table = $relation["table"];
+            $foreignKey = $relation["foreignKey"] ?? "{$relation["name"]}_id";
+            $localKey = $relation["localKey"] ?? "id";
+
+            $query->$method(
+                $table,
+                "{$table}.{$localKey}",
+                "=",
+                "{$query->getModel()->getTable()}.{$foreignKey}"
+            );
+        }
+
+        return $query;
+    }
+
+    public static function buildSelects(
+        array $relations,
+        array $mainTableFields
+    ) {
+        $selects = $mainTableFields;
+        foreach ($relations as $relation) {
+            $relationName = $relation["name"];
+            $relationProperties = $relation["properties"];
+
+            foreach ($relationProperties as $property) {
+                $selects[] = "{$relation["table"]}.{$property} as {$relationName}_{$property}";
+            }
+        }
+        return $selects;
+    }
+
+    public static function transformWithRelations($items, $relations)
+    {
+        return $items->map(function ($item) use ($relations) {
+            foreach ($relations as $relation) {
+                $relationName = $relation["name"];
+                $relationProperties = $relation["properties"];
+
+                $nested = [];
+                foreach ($relationProperties as $property) {
+                    $nested[$property] =
+                        $item->{$relationName . "_" . $property};
+                    unset($item->{$relationName . "_" . $property});
+                }
+                $item->{$relationName} = $nested;
+            }
+            return $item;
+        });
+    }
+}
